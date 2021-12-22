1bd6df05d91c8aea99409263d602d2076272e68e
Eli McNutt <eli.mcnutt@gmail.com>
2021-12-22 15:12:17 -0800

    Updates to emulate script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,24 +1,34 @@
 import "@typechain/hardhat"
 import "@nomiclabs/hardhat-ethers"
 import "@nomiclabs/hardhat-waffle"
-import { HardhatUserConfig } from "hardhat/types"
+import {HardhatUserConfig} from "hardhat/types"
 import "tsconfig-paths/register"
 import "hardhat-contract-sizer"
 
-const config: HardhatUserConfig = {
-    solidity: "0.8.8",
-    networks: {
-        localhost  : {
-            allowUnlimitedContractSize: true,
-        },
-        hardhat  : {
-            allowUnlimitedContractSize: true,
-            chainId: 31337,
-        },
-    },
-    mocha: {
-        timeout: 150000
-    }
-}
+import emulate from "./scripts/emulate";
+import {task} from "hardhat/internal/core/config/config-env";
+import fs from "fs";
 
+task("emulate", "Play through the game.")
+  .addParam("seizes", "The number of seizes to emulate.")
+  .setAction(async (args, hre) => {
+    const {seizes} = args
+    await emulate(seizes, hre)
+  })
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.8",
+  networks: {
+    localhost: {
+      allowUnlimitedContractSize: true,
+    },
+    hardhat: {
+      allowUnlimitedContractSize: true,
+      chainId: 31337,
+    },
+  },
+  mocha: {
+    timeout: 150000
+  }
+}
 export default config
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,29 +1,31 @@
 import {ethers} from "hardhat"
-import ErrnoException = NodeJS.ErrnoException;
 import * as fs from "fs";
-
+import * as logger from "./logger"
 
 export async function deploy() {
-
+  logger.divider()
+  logger.out('Starting contract deploy...', logger.Level.Info)
+  logger.divider()
 
   const [deployer] = await ethers.getSigners()
+
   // Deploy the NFT contract
   const Seekers = await ethers.getContractFactory("Seekers")
   const seekers = await Seekers.deploy()
   await seekers.deployed()
-  console.log("Seeker contract address", seekers.address)
+  logger.pad(30, 'Seekers contract:', seekers.address)
 
   // Deploy the Keepers Vault contract 
   const Vault = await ethers.getContractFactory("Vault")
   const vault = await Vault.deploy(seekers.address)
   await vault.deployed()
-  console.log("Vault address", vault.address)
+  logger.pad(30, 'Vault contract:', vault.address)
 
   // Deploy the Coin contract 
   const SeasonOne = await ethers.getContractFactory("SeasonOne")
   const seasonOne = await SeasonOne.deploy(seekers.address, vault.address)
   await seasonOne.deployed()
-  console.log("CoinOne address", seasonOne.address)
+  logger.pad(30, 'SeasonOne contract:', seasonOne.address)
 
   // Establish permissions for SeasonOne contract within Seekers contract
   await seekers.addGameContract(seasonOne.address)
@@ -32,27 +34,33 @@ export async function deploy() {
   const ERC721Marketplace = await ethers.getContractFactory("ERC721Marketplace")
   const erc721Marketplace = await ERC721Marketplace.deploy()
   await erc721Marketplace.deployed()
-  console.log("ERC721Marketplace address", erc721Marketplace.address)
+  logger.pad(30, 'ERC721Marketplace contract:', erc721Marketplace.address)
 
   // Deploy the Sft Marketplace contract
   const ERC1155Marketplace = await ethers.getContractFactory("ERC1155Marketplace")
   const erc1155Marketplace = await ERC1155Marketplace.deploy()
   await erc1155Marketplace.deployed()
-  console.log("ERC1155Marketplace address", erc1155Marketplace.address)
+  logger.pad(30, 'ERC1155Marketplace contract:', erc1155Marketplace.address)
 
-  // Build local json file. Used to store contract data
+  logger.divider()
+  logger.out('Exporting contract address data...')
+  logger.divider()
+
+  // Build local json file. Used to store address contract data
   const data = {
     contracts: {
       "seekers": seekers.address,
       "vault": vault.address,
       "seasonOne": seasonOne.address,
-      "nft-marketplace": erc721Marketplace.address,
-      "sft-marketplace": erc1155Marketplace.address,
+      "nftMarketplace": erc721Marketplace.address,
+      "sftMarketplace": erc1155Marketplace.address,
     }
   }
-  
   const json = JSON.stringify(data, null, 2)
-  fs.writeFileSync('addresses.json', json, "utf8")
+  fs.writeFileSync('local/addresses.json', json, "utf8")
+
+  logger.out('Deploy complete!', logger.Level.Info)
+  logger.divider()
 }
 
 deploy()
