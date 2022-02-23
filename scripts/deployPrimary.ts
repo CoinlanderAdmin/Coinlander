0335e11f2f9650d76a5a3a0b687625ad65d88658
katzman <steve.r.katzman@gmail.com>
2022-02-23 19:55:17 +0000

    Autonomous commit for deploy at 2022-02-23 19:55:17 on 421611

--- scripts/deployPrimary.ts
+++ scripts/deployPrimary.ts
@@ -17,60 +17,60 @@ export async function deploy() {
   logger.out("Deploying to: " + network.name, logger.Level.Info)
   logger.out("With chain id: " + network.chainId, logger.Level.Info)
   
-  for(let i = 0; i < 3; i ++){
-    logger.divider()
-    logger.out('Starting contract deploy...', logger.Level.Info)
-    logger.divider()
+  // for(let i = 0; i < 3; i ++){
+  //   logger.divider()
+  //   logger.out('Starting contract deploy...', logger.Level.Info)
+  //   logger.divider()
 
-    const [deployer] = await ethers.getSigners()
-    const deployBlock = await (await ethers.provider.getBlock("latest")).number
+  //   const [deployer] = await ethers.getSigners()
+  //   const deployBlock = await (await ethers.provider.getBlock("latest")).number
 
-    // Deploy the NFT contract
-    const Seekers = await ethers.getContractFactory("Seekers")
-    const seekers = await Seekers.deploy()
-    await seekers.deployed()
-    logger.pad(30, 'Seekers contract:', seekers.address)
+  //   // Deploy the NFT contract
+  //   const Seekers = await ethers.getContractFactory("Seekers")
+  //   const seekers = await Seekers.deploy()
+  //   await seekers.deployed()
+  //   logger.pad(30, 'Seekers contract:', seekers.address)
 
-    // Deploy the Keepers Vault contract 
-    const Vault = await ethers.getContractFactory("Vault")
-    const vault = await Vault.deploy(seekers.address)
-    await vault.deployed()
-    logger.pad(30, 'Vault contract:', vault.address)
+  //   // Deploy the Keepers Vault contract 
+  //   const Vault = await ethers.getContractFactory("Vault")
+  //   const vault = await Vault.deploy(seekers.address)
+  //   await vault.deployed()
+  //   logger.pad(30, 'Vault contract:', vault.address)
 
-    // Deploy the Coin contract 
-    const SeasonOne = await ethers.getContractFactory("SeasonOne")
-    const seasonOne = await SeasonOne.deploy(seekers.address, vault.address)
-    await seasonOne.deployed()
-    logger.pad(30, 'SeasonOne contract:', seasonOne.address)
+  //   // Deploy the Coin contract 
+  //   const SeasonOne = await ethers.getContractFactory("SeasonOne")
+  //   const seasonOne = await SeasonOne.deploy(seekers.address, vault.address)
+  //   await seasonOne.deployed()
+  //   logger.pad(30, 'SeasonOne contract:', seasonOne.address)
 
-    // Establish permissions for SeasonOne contract within Seekers contract
-    await seekers.addGameContract(seasonOne.address)
+  //   // Establish permissions for SeasonOne contract within Seekers contract
+  //   await seekers.addGameContract(seasonOne.address)
 
-    // Set SeasonOne contract as owner of vault contract
-    await vault.transferOwnership(seasonOne.address)
+  //   // Set SeasonOne contract as owner of vault contract
+  //   await vault.transferOwnership(seasonOne.address)
 
-    data[i] = {
-      "deployBlock": deployBlock,
-      contracts: {
-        "seekers": seekers.address,
-        "vault": vault.address,
-        "seasonOne": seasonOne.address
-      }
-    }
+  //   data[i] = {
+  //     "deployBlock": deployBlock,
+  //     contracts: {
+  //       "seekers": seekers.address,
+  //       "vault": vault.address,
+  //       "seasonOne": seasonOne.address
+  //     }
+  //   }
 
-  }
+  // }
 
-  logger.divider()
-  logger.out('Exporting contract address data...')
-  logger.divider()
+  // logger.divider()
+  // logger.out('Exporting contract address data...')
+  // logger.divider()
 
-  // Build local json file. Used to store address contract data
+  // // Build local json file. Used to store address contract data
   
-  const json = JSON.stringify(data, null, 2)
-  fs.writeFileSync('addresses.json', json, "utf8")
+  // const json = JSON.stringify(data, null, 2)
+  // fs.writeFileSync('addresses.json', json, "utf8")
 
-  logger.out('Deploy complete!', logger.Level.Info)
-  logger.divider()
+  // logger.out('Deploy complete!', logger.Level.Info)
+  // logger.divider()
 
   if(network.chainId == 421611) {
     logger.divider()
